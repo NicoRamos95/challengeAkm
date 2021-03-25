eb3a6338e5ffd0527569758eb4ce80e8c33b3e3d
NicoRamos95 <nicom.ramos@hotmail.com>
2021-03-25 16:32:27 -0300

    add-frontend

---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,19 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <div>
+                <div>
+                    <p>Email</p>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <p>Password</p>
+                    <input type="text"/>
+                </div>
+                <button>Logearse</button>
+            </div>
+        </div>
+    )
+}
+export default Login
